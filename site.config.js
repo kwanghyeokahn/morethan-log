b401422630d3fda74770194415e4750960e2cc7e
KwangHyeokAhn <44485235+kwanghyeokahn@users.noreply.github.com>
2024-01-15 17:06:16 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -29,7 +29,7 @@ const CONFIG = {
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo : {
-    keywords: ["Blog","website","Notion","Python","Recipe","eyelight-log"]
+    keywords: ["Blog","website","Notion","vercel","morethan-log","Python","Recipe","eyelight-log"]
   },
   // notion configuration (required)
   notionConfig: {
