af771585225f5e781585b3c01874bdabcb6eb3d8
KwangHyeokAhn <44485235+kwanghyeokahn@users.noreply.github.com>
2024-02-02 15:11:08 +0900

    Update getAllSelectItemsFromPosts.ts
    tags 기준 category 보기

--- src/libs/utils/notion/getAllSelectItemsFromPosts.ts
+++ src/libs/utils/notion/getAllSelectItemsFromPosts.ts
@@ -1,10 +1,31 @@
+//import { TPosts } from "src/types"
+
+//export function getAllSelectItemsFromPosts(
+//  key: "tags" | "category",
+//  posts: TPosts
+//) {
+//  const selectedPosts = posts.filter((post) => post?.[key])
+//  const items = [...selectedPosts.map((p) => p[key]).flat()]
+//  const itemObj: { [itemName: string]: number } = {}
+//  items.forEach((item) => {
+//    if (!item) return
+//    if (item in itemObj) {
+//      itemObj[item]++
+//    } else {
+//      itemObj[item] = 1
+//    }
+//  })
+//  return itemObj
+//}
+
+
 import { TPosts } from "src/types"
 
 export function getAllSelectItemsFromPosts(
   key: "tags" | "category",
   posts: TPosts
 ) {
-  const selectedPosts = posts.filter((post) => post?.[key])
+  const selectedPosts = posts.filter((post) => post?.tags?.includes("category")) // tags 값에 "category"가 포함된 포스트들을 필터링합니다.
   const items = [...selectedPosts.map((p) => p[key]).flat()]
   const itemObj: { [itemName: string]: number } = {}
   items.forEach((item) => {
